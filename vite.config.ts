9e9f7f94cba935d13ec4b0cc777cb47b5cafefff
blackvrice <45651641+blackvrice@users.noreply.github.com>
2025-12-07 14:09:26 +0900

    Update vite.config.ts
    경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base : process.env.GITHUB_ACTIONS ? '/Profile/' : '/',
+    base : process.env.GITHUB_ACTIONS ? '/profile/' : '/',
     plugins: [react()],
     server: {
         proxy: {
